3db5f5d997dbf10fb5f0e2035f56e3ca7d2d0209
khushboo.srivastava <khushboo.srivastava@construction.com>
2017-09-21 09:14:43 +0000

    local commit

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -11,7 +11,7 @@ public class LocalTest extends BrowserStackTestNGTest {
         driver.get("http://bs-local.com:45691/check");
         System.out.println("This ismy local test!!!");
         System.out.println("This ismy local test111!!!");
-        
+        System.out.println("This ismy local commit test1123!!!");
         Assert.assertTrue(driver.getPageSource().contains("Up and running"));
     }
 }
