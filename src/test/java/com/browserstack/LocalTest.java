cb9cad382c0146d4dc88040ec9a5fac5fe6bce01
khushboo.srivastava <khushboo.srivastava@construction.com>
2017-09-21 09:24:23 +0000

    Revert "test test test"
    This reverts commit 08ecb2eaa0b1cb87040f638f96b12bceb23d83ed.
    
    # Conflicts:
    #	src/test/java/com/browserstack/LocalTest.java

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -10,8 +10,6 @@ public class LocalTest extends BrowserStackTestNGTest {
     public void test() throws Exception {
         driver.get("http://bs-local.com:45691/check");
         System.out.println("This ismy local test!!!");
-        System.out.println("This ismy local test111!!!");
-        System.out.println("This ismy local commit test1123!!!");
         Assert.assertTrue(driver.getPageSource().contains("Up and running"));
     }
 }
