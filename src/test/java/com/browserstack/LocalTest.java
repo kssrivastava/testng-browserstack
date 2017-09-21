390e636411b75c57b9e92c8a5d219ff503f50b7b
khushboo.srivastava <khushboo.srivastava@construction.com>
2017-09-21 09:12:09 +0000

    test11

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -10,6 +10,8 @@ public class LocalTest extends BrowserStackTestNGTest {
     public void test() throws Exception {
         driver.get("http://bs-local.com:45691/check");
         System.out.println("This ismy local test!!!");
+        System.out.println("This ismy local test111!!!");
+        
         Assert.assertTrue(driver.getPageSource().contains("Up and running"));
     }
 }
