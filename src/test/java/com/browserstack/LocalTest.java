08ecb2eaa0b1cb87040f638f96b12bceb23d83ed
khushboo.srivastava <khushboo.srivastava@construction.com>
2017-09-21 09:10:53 +0000

    test test test

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -1,5 +1,6 @@
 package com.browserstack;
 
+import org.jboss.netty.util.internal.SystemPropertyUtil;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -8,7 +9,7 @@ public class LocalTest extends BrowserStackTestNGTest {
     @Test
     public void test() throws Exception {
         driver.get("http://bs-local.com:45691/check");
-
+        System.out.println("This ismy local test!!!");
         Assert.assertTrue(driver.getPageSource().contains("Up and running"));
     }
 }
