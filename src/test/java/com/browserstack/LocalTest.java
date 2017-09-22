f99db774a1e22a1694d4d066a7539ef318dc7262
khushboo.srivastava <khushboo.srivastava@construction.com>
2017-09-22 12:12:38 +0000

    access qa3 in test project

--- src/test/java/com/browserstack/LocalTest.java
+++ src/test/java/com/browserstack/LocalTest.java
@@ -6,10 +6,16 @@ import org.testng.annotations.Test;
 
 public class LocalTest extends BrowserStackTestNGTest {
 
-    @Test
+   /* @Test
     public void test() throws Exception {
         driver.get("http://bs-local.com:45691/check");
         System.out.println("This ismy local test!!!");
         Assert.assertTrue(driver.getPageSource().contains("Up and running"));
-    }
+    }*/
+	 @Test
+	    public void test() throws Exception {
+	        driver.get("http://network2-qa3.construction.com");
+	        System.out.println("This is my qa3 local test!!!");
+	        Assert.assertTrue(driver.getPageSource().contains("Sign In"));
+	    }
 }
